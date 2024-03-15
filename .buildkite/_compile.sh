71830107b9d793bc8b4ee955b62f9420112a80a0
Craig de Stigter <craig@destigter.nz>
2024-03-15 13:15:06 +1300

    Build against latest jammy gdal

--- .buildkite/_compile.sh
+++ .buildkite/_compile.sh
@@ -10,17 +10,9 @@ apt-get install -y --no-install-recommends \
     file \
     libcurl4-openssl-dev \
     libgeotiff-dev \
-    libgdal-dev
-
-declare -a CMAKE_EXTRA=()
-
-if [ "$DISTRIB_CODENAME" == "bionic" ]; then
-    apt-get install -y --no-install-recommends gcc-8 g++-8
-    CMAKE_EXTRA+=(
-        "-DCMAKE_C_COMPILER=gcc-8"
-        "-DCMAKE_CXX_COMPILER=g++-8"
-    )
-fi
+    libgdal-dev \
+    gdal-plugins \
+    gdal-data
 
 # install modern cmake
 CMAKE_VER=3.24.1
@@ -35,7 +27,6 @@ cd /mnt/build
 # configure
 echo "+++ Configuring..."
 cmake -S /src -B . \
-    "${CMAKE_EXTRA[@]}" \
     -DCMAKE_BUILD_TYPE=Release \
     -DBUILD_PLUGIN_PGPOINTCLOUD=OFF \
     -DWITH_TESTS=NO \
@@ -43,7 +34,7 @@ cmake -S /src -B . \
     -DCPACK_DEBIAN_PACKAGE_MAINTAINER=robert.coup@koordinates.com \
     -DCPACK_DEBIAN_PACKAGE_SHLIBDEPS=ON \
     -DCPACK_DEBIAN_PACKAGE_GENERATE_SHLIBS=ON \
-    -DCPACK_DEBIAN_PACKAGE_GENERATE_SHLIBS_POLICY="=" \
+    -DCPACK_DEBIAN_PACKAGE_GENERATE_SHLIBS_POLICY=">=" \
     -DCPACK_DEBIAN_FILE_NAME=DEB-DEFAULT
 
 # compile
